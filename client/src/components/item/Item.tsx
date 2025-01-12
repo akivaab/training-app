d77e79c9152cf2d5be9805d9ff350d45bf639985
akivaab <amabramo18@gmail.com>
2025-01-12 16:04:12 +0200

    item start

--- client/src/components/item/Item.tsx
+++ client/src/components/item/Item.tsx
@@ -1,5 +1,35 @@
+// import { useEffect, useState } from "react";
+// import { useNavigate, useParams } from "react-router-dom";
+// import { ItemType } from "../../api/itemsApi";
+
 function Item() {
-  return <div></div>;
+  // const navigate = useNavigate();
+  // const { id } = useParams();
+  // const [item, setItem] = useState<ItemType | null>(null);
+
+  // useEffect(() => {
+  //   handleGetItem();
+  // }, []);
+
+  // async function handleGetItem() {
+  //   const data = await getItem(id);
+  //   if (data) {
+  //     setItem(data);
+  //   }
+  //   //setIsLoading(false);
+  // }
+
+  return (
+    <div>
+      <div>
+        <h2></h2>
+      </div>
+      <div className="bg-teal-900">
+        <button>Edit</button>
+        <button>Delete</button>
+      </div>
+    </div>
+  );
 }
 
 export default Item;
