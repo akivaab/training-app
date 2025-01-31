40e4af63028ce63949ea14b264c48f46f026cd2a
akivaab <amabramo18@gmail.com>
2025-01-31 10:20:36 +0200

    error handler handles more

--- server/middleware/errorHandler.ts
+++ server/middleware/errorHandler.ts
@@ -1,4 +1,9 @@
+import { QueryError } from "mysql2";
 import { Request, Response, NextFunction } from "express";
+
+function isQueryError(err: Error): err is QueryError {
+  return err && typeof err === "object" && "errno" in err;
+}
 
 export default function errorHandler(
   err: Error,
@@ -6,5 +11,12 @@ export default function errorHandler(
   res: Response,
   next: NextFunction
 ): void {
-  res.status(500).json({ message: err.message });
+  // Duplicate entry
+  if (isQueryError(err) && err.errno === 1062) {
+    res
+      .status(409)
+      .json({ message: "User with this email or phone is already registered" });
+  } else {
+    res.status(500).json({ message: "Unknown internal server error" });
+  }
 }
